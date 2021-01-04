24c64673374a6558aa203dd0a2de6aee8f76fb30
typhonrt <support@typhonrt.org>
2021-01-04 01:17:54 -0800

    updated to typhonjs-color-logger

--- src/hooks/init/init.js
+++ src/hooks/init/init.js
@@ -57,8 +57,7 @@ module.exports = async function(opts)
       // Adds flags for various built in commands like `build`.
       s_ADD_FLAGS(opts.id);
 
-      // TODO REMOVE
-      process.stdout.write(`plugin-replace init hook running ${opts.id}\n`);
+      global.$$eventbus.trigger('log:debug', `plugin-replace init hook running '${opts.id}'.`);
    }
    catch (error)
    {
