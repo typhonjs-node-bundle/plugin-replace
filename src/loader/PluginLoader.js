15c7424716b53bf82a9ba233298a0ee8cbb742e2
typhonrt <support@typhonrt.org>
2021-01-09 02:46:36 -0800

    refactor / cleanup

--- src/loader/PluginLoader.js
+++ src/loader/PluginLoader.js
@@ -4,6 +4,9 @@ const replace           = require('@rollup/plugin-replace');
 
 const { NonFatalError } = require('@typhonjs-node-bundle/oclif-commons');
 
+const s_CONFLICT_PACKAGES = ['@rollup/plugin-replace'];
+const s_PACKAGE_NAME = '@typhonjs-node-rollup/plugin-replace';
+
 /**
  * Handles interfacing with the plugin manager adding event bindings to pass back a configured
  * instance of `@rollup/plugin-replace`.
@@ -11,21 +14,21 @@ const { NonFatalError } = require('@typhonjs-node-bundle/oclif-commons');
 class PluginLoader
 {
    /**
+    * Returns the any modules that cause a conflict.
+    *
+    * @returns {string[]}
+    */
+   static get conflictPackages() { return s_CONFLICT_PACKAGES; }
+
+   /**
     * Returns the `package.json` module name.
     *
     * @returns {string}
     */
-   static get pluginName() { return '@typhonjs-node-rollup/plugin-replace'; }
+   static get packageName() { return s_PACKAGE_NAME; }
 
    /**
-    * Returns the rollup plugins managed.
-    *
-    * @returns {string[]}
-    */
-   static get rollupPlugins() { return ['@rollup/plugin-replace']; }
-
-   /**
-    * Adds flags for various built in commands like `build`.
+    * Adds flags for various built in commands like `bundle`.
     *
     * To add handling of the *.env environment variables a double processing stage occurs in fvttdev build command. The
     * flags are processed to pull out the --env flag then if present `dotenv` is used to load the given *.env file.
@@ -40,126 +43,119 @@ class PluginLoader
     * Added flags include:
     * `--replace`   - `-r` - Replace constants with hard-coded values.  - default:           - env: {prefix}_REPLACE
     *
-    * @param {string} command - ID of the command being run.
     * @param {object} eventbus - The eventbus to add flags to.
     */
-   static addFlags(command, eventbus)
+   static addFlags(eventbus)
    {
-      switch (command)
-      {
-         // Add all built in flags for the build command.
-         case 'bundle':
-            eventbus.trigger('typhonjs:oclif:system:flaghandler:add', {
-               command,
-               plugin: PluginLoader.pluginName,
-               flags: {
-                  replace: flags.string({
-                     'char': 'r',
-                     'description': 'Replace constants with hard-coded values.',
-                     'multiple': true,
-                     'default': function()
-                     {
-                        const envVar = `${global.$$flag_env_prefix}_REPLACE`;
-
-                        if (typeof process.env[envVar] === 'string')
-                        {
-                           let result = void 0;
-
-                           // Treat it as a JSON array.
-                           try { result = JSON.parse(process.env[envVar]); }
-                           catch (error)
-                           {
-                              throw new NonFatalError(
-                               `Could not parse '${envVar}' as a JSON array;\n${error.message}`);
-                           }
-
-                           // Verify that the JSON result loaded is an actual array otherwise quit with and error...
-                           if (!Array.isArray(result))
-                           {
-                              throw new NonFatalError(`Please format '${envVar}' as a JSON array.`);
-                           }
-
-                           // TODO: consider adding verification that the loaded array from JSON contains all strings.
-
-                           return result;
-                        }
-
-                        return void 0;
-                     }
-                  })
-               },
-
-               /**
-                * Verifies the `replace` flag and checks that the data loaded is an array, and then attempts to parse
-                * each entry. If an entry is not a string in the format of <xxx>=<yyy> an error is generated. An error
-                * is also generated if an entry overwrites a previous entry which occurs when there are multiple left
-                * hand values of the same string.
-                *
-                * @param {object}   flags - The CLI flags to verify.
-                */
-               verify: function(flags)
+      eventbus.trigger('typhonjs:oclif:system:flaghandler:add', {
+         command: 'bundle',
+         pluginName: PluginLoader.packageName,
+         flags: {
+            replace: flags.string({
+               'char': 'r',
+               'description': 'Replace constants with hard-coded values.',
+               'multiple': true,
+               'default': function()
                {
-                  const regex = /(.+)=(.+)/;
+                  const envVar = `${global.$$flag_env_prefix}_REPLACE`;
 
-                  // replace should always be an array
-                  if (Array.isArray(flags.replace))
+                  if (typeof process.env[envVar] === 'string')
                   {
-                     const badEntries = [];
-                     const warnEntries = [];
+                     let result = void 0;
 
-                     const entries = {};
-
-                     flags.replace.forEach((entry) =>
+                     // Treat it as a JSON array.
+                     try { result = JSON.parse(process.env[envVar]); }
+                     catch (error)
                      {
-                        const matches = regex.exec(entry);
-
-                        if (matches !== null && matches.length >= 3)
-                        {
-                           // If the left hand match is already in the entries object as a key then add the current
-                           // entry to the warn list.
-                           if (matches[1] in entries)
-                           {
-                              warnEntries.push(entry);
-                           }
-                           else
-                           {
-                              entries[matches[1]] = matches[2];
-                           }
-                        }
-                        else
-                        {
-                           badEntries.push(entry);
-                        }
-                     });
-
-                     flags.replace = entries;
-
-                     // TODO: TEMPORARILY ADD DELIMITERS; REMOVE WHEN SWITCH TO `env-resolve`.
-                     flags.replace.delimiters = ['', ''];
-
-                     let errorMessage = 'plugin-replace verification failure:\n';
-
-                     if (badEntries.length > 0)
-                     {
-                        errorMessage += `- can not parse ${JSON.stringify(badEntries)} each `
-                         + `entry must be a 'string' in the format of '<xxx>=<yyy>'.`;
+                        throw new NonFatalError(
+                         `Could not parse '${envVar}' as a JSON array;\n${error.message}`);
                      }
 
-                     if (warnEntries.length > 0)
+                     // Verify that the JSON result loaded is an actual array otherwise quit with and error...
+                     if (!Array.isArray(result))
                      {
-                        errorMessage += `${badEntries.length > 0 ? '\n' : ''}- the following `
-                         + `entries overwrite previous entries ${JSON.stringify(warnEntries)}.`;
+                        throw new NonFatalError(`Please format '${envVar}' as a JSON array.`);
                      }
 
-                     if (errorMessage !== 'plugin-replace verification failure:\n')
+                     // TODO: consider adding verification that the loaded array from JSON contains all strings.
+
+                     return result;
+                  }
+
+                  return void 0;
+               }
+            })
+         },
+
+         /**
+          * Verifies the `replace` flag and checks that the data loaded is an array, and then attempts to parse
+          * each entry. If an entry is not a string in the format of <xxx>=<yyy> an error is generated. An error
+          * is also generated if an entry overwrites a previous entry which occurs when there are multiple left
+          * hand values of the same string.
+          *
+          * @param {object}   flags - The CLI flags to verify.
+          */
+         verify: function(flags)
+         {
+            const regex = /(.+)=(.+)/;
+
+            // replace should always be an array
+            if (Array.isArray(flags.replace))
+            {
+               const badEntries = [];
+               const warnEntries = [];
+
+               const entries = {};
+
+               flags.replace.forEach((entry) =>
+               {
+                  const matches = regex.exec(entry);
+
+                  if (matches !== null && matches.length >= 3)
+                  {
+                     // If the left hand match is already in the entries object as a key then add the current
+                     // entry to the warn list.
+                     if (matches[1] in entries)
                      {
-                        throw new NonFatalError(errorMessage);
+                        warnEntries.push(entry);
+                     }
+                     else
+                     {
+                        entries[matches[1]] = matches[2];
                      }
                   }
+                  else
+                  {
+                     badEntries.push(entry);
+                  }
+               });
+
+               flags.replace = entries;
+
+               // TODO: TEMPORARILY ADD DELIMITERS; REMOVE WHEN SWITCH TO `env-resolve`.
+               flags.replace.delimiters = ['', ''];
+
+               let errorMessage = 'plugin-replace verification failure:\n';
+
+               if (badEntries.length > 0)
+               {
+                  errorMessage += `- can not parse ${JSON.stringify(badEntries)} each `
+                   + `entry must be a 'string' in the format of '<xxx>=<yyy>'.`;
                }
-            });
-            break;
-      }
+
+               if (warnEntries.length > 0)
+               {
+                  errorMessage += `${badEntries.length > 0 ? '\n' : ''}- the following `
+                   + `entries overwrite previous entries ${JSON.stringify(warnEntries)}.`;
+               }
+
+               if (errorMessage !== 'plugin-replace verification failure:\n')
+               {
+                  throw new NonFatalError(errorMessage);
+               }
+            }
+         }
+      });
    }
 
    /**
@@ -191,7 +187,7 @@ class PluginLoader
    {
       ev.eventbus.on('typhonjs:oclif:bundle:plugins:main:input:get', PluginLoader.getInputPlugin, PluginLoader);
 
-      PluginLoader.addFlags(ev.pluginOptions.id, ev.eventbus);
+      PluginLoader.addFlags(ev.eventbus);
    }
 }
 
